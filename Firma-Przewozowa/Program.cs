223db7d15b9c016c141adced5acd0c2d818f85ec
jurasafc <jurasafc@gmail.com>
2019-12-14 13:34:27 +0100

    Nowa gałąź. Dodanie klasy Pojazd.

--- Firma-Przewozowa/Program.cs
+++ Firma-Przewozowa/Program.cs
@@ -12,4 +12,16 @@ namespace Firma_Przewozowa
         {
         }
     }
+    public class Pojazd
+    {
+        public int rocznik;
+        public int ilosc_miejsc;
+        public int przebieg;
+        public Pojazd(int r, int i, int p)
+        {
+            this.rocznik = r;
+            this.ilosc_miejsc = i;
+            this.przebieg = p;
+        }
+    }
 }
