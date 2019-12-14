f9ccd2c77fa827a1b0719332baffbfe116f65ac8
jurasafc <jurasafc@gmail.com>
2019-12-14 13:47:19 +0100

    Nowa gałąź. Dodanie klasy Firma.

--- Firma-Przewozowa/Program.cs
+++ Firma-Przewozowa/Program.cs
@@ -12,4 +12,15 @@ namespace Firma_Przewozowa
         {
         }
     }
+    public class Firma
+    {
+        public string nazwa;
+        public int budzet;
+        public string siedziba;
+        public Firma(string n, string s)
+        {
+            this.nazwa = n;
+            this.siedziba = s;
+        }
+    }
 }
