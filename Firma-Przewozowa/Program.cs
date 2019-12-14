e79f99d6ed678ff8c951c6dd63b06ce7b06cc3dd
jurasafc <jurasafc@gmail.com>
2019-12-14 13:27:08 +0100

    Nowa gałąź. Dodanie klasy 'Osoba'.

--- Firma-Przewozowa/Program.cs
+++ Firma-Przewozowa/Program.cs
@@ -12,4 +12,16 @@ namespace Firma_Przewozowa
         {
         }
     }
+    public class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int wiek;
+        public Osoba(string i, string n, int w)
+        {
+            this.imie = i;
+            this.nazwisko = n;
+            this.wiek = w;
+        }
+    }
 }
