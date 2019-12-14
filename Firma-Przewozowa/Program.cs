d851017b093dd5f56ff01605fe30429840ff89bc
jurasafc <jurasafc@gmail.com>
2019-12-14 13:52:47 +0100

    Nowa gałąź. Dodanie klasy MIejscowość.

--- Firma-Przewozowa/Program.cs
+++ Firma-Przewozowa/Program.cs
@@ -12,4 +12,16 @@ namespace Firma_Przewozowa
         {
         }
     }
+    public class Miejscowosc
+    {
+        string nazwa;
+        int odleglosc_km;
+        int czas_przejazdu_h;
+        public Miejscowosc(string n, int o)
+        {
+            this.nazwa = n;
+            this.odleglosc_km = o;
+            this.czas_przejazdu_h = this.odleglosc_km / 60;
+        }
+    }
 }
